01b02add3be457324d1e563b089f503dea4e0b86
capricornus44 <antonzavalniuk@gmail.com>
2021-05-26 14:40:57 +0200

    Step 2-3: created db with contacts.json,  contacts.js with function to wotk with contacts collection

---
+++ contacts.js
@@ -0,0 +1,48 @@
+const fs = require("fs").promises
+const path = require("path")
+const { nanoid } = require("nanoid")
+
+const contactsPath = path.basename("./db/contacts.json")
+// const contactsPath = path.join(__dirname, "./db/contacts.json")
+
+// ** Function to work with contacts collection
+async function listContacts() {
+  return JSON.parse(await fs.readFile(contactsPath, "utf-8"))
+}
+
+async function getContactById(contactId) {
+  try {
+    const list = await listContacts()
+    return list.find((el) => el.id === contactId)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+async function removeContact(contactId) {
+  try {
+    const list = await listContacts()
+    const res = list.filter((el) => el.id !== contactId)
+    await fs.writeFile(contactsPath, JSON.stringify(res))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+async function addContact(name, email, phone) {
+  const id = nanoid(4)
+
+  try {
+    const list = await listContacts()
+    await fs.writeFile(contactsPath, JSON.stringify([...list, { id, name, email, phone }]))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+}
