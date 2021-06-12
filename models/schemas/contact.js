09b9054f9047be9846c9990bf26adb6a89ab84ce
capricornus44 <antonzavalniuk@gmail.com>
2021-06-12 22:25:37 +0200

    updated contactSchema

--- models/schemas/contact.js
+++ models/schemas/contact.js
@@ -1,26 +1,29 @@
 const { Schema } = require("mongoose")
 
-const contactSchema = Schema({
-  name: {
-    type: String,
-    required: [true, "Missing required name field"],
-    minLength: 2,
-    maxLength: 50,
+const contactSchema = Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Missing required name field"],
+      minLength: 2,
+      maxLength: 50,
+    },
+    email: {
+      type: String,
+      required: [true, "Missing required email field"],
+      unique: true,
+    },
+    phone: {
+      type: String,
+      required: [true, "Missing required phone field"],
+      unique: true,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
   },
-  email: {
-    type: String,
-    required: [true, "Missing required email field"],
-    unique: true,
-  },
-  phone: {
-    type: String,
-    required: [true, "Missing required phone field"],
-    unique: true,
-  },
-  favorite: {
-    type: Boolean,
-    default: false,
-  },
-})
+  { versionKey: false, timestamps: true }
+)
 
 module.exports = contactSchema
