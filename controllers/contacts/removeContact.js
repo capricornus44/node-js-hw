098e3330b2fcab0f711ede2f5d24de631455f85b
capricornus44 <antonzavalniuk@gmail.com>
2021-06-12 17:16:03 +0200

    replaced CRUD-operation code with mongoose-method for removeContact controller

--- controllers/contacts/removeContact.js
+++ controllers/contacts/removeContact.js
@@ -1,22 +1,42 @@
-const { contactsRequestFunctions: db } = require("../../helpers")
+// const { contactsRequestFunctions: db } = require("../../helpers")
+
+// const removeContact = async (req, res, next) => {
+//   try {
+//     const { contactId } = req.params
+//     const contact = await db.removeContact(contactId)
+
+//     if (!contact) {
+//       return res.status(404).json({
+//         status: "error",
+//         code: 404,
+//         message: "Contact not found",
+//       })
+//     }
+
+//     res.status(200).json({
+//       status: "success",
+//       code: 200,
+//       message: "Contact deleted",
+//     })
+//   } catch (error) {
+//     next(error)
+//   }
+// }
+
+// module.exports = removeContact
+
+const { contact: service } = require("../../services")
 
 const removeContact = async (req, res, next) => {
+  const { contactId } = req.params
   try {
-    const { contactId } = req.params
-    const contact = await db.removeContact(contactId)
-
-    if (!contact) {
-      return res.status(404).json({
-        status: "error",
-        code: 404,
-        message: "Contact not found",
-      })
-    }
-
-    res.status(200).json({
+    const result = await service.removeContact(contactId)
+    res.status(201).json({
       status: "success",
-      code: 200,
-      message: "Contact deleted",
+      code: 201,
+      data: {
+        result,
+      },
     })
   } catch (error) {
     next(error)
