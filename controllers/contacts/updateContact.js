c4c7365c22faa05b6a2d413f4c3ac38bf45d84be
capricornus44 <antonzavalniuk@gmail.com>
2021-06-12 11:08:10 +0200

    added .idea to gitignore

--- controllers/contacts/updateContact.js
+++ controllers/contacts/updateContact.js
@@ -14,14 +14,6 @@ const updateContact = async (req, res, next) => {
       })
     }
 
-    if (!valueToUpdate.name || !valueToUpdate.email || !valueToUpdate.phone) {
-      return res.status(400).json({
-        status: "error",
-        code: 400,
-        message: "Missing fields",
-      })
-    }
-
     res.status(200).json({
       status: "success",
       code: 200,
